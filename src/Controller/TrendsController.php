1f422f35059fb74a643abac5b9f60008541ff7e1
Chastaingt <thomas-chastaingt@outlook.com>
2020-06-22 17:37:33 +0200

    add trends view

--- src/Controller/TrendsController.php
+++ src/Controller/TrendsController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PollsRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 class TrendsController extends AbstractController
 {
     /**
      * @Route("/trends", name="trends")
      */
-    public function index()
+    public function index(PollsRepository $pollsRepository):Response
     {
         return $this->render('trends/index.html.twig', [
-            'controller_name' => 'TrendsController',
+            'polls' => $pollsRepository->findAll(),
         ]);
     }
 }
