70a1897d62ea189f6a28eca0f2ce269a9711e2cb
Chastaingt <thomas-chastaingt@outlook.com>
2020-06-22 15:35:27 +0200

    add styles and sheets

--- src/Security/LogginFormAuthenticator.php
+++ src/Security/LogginFormAuthenticator.php
@@ -96,7 +96,7 @@ class LogginFormAuthenticator extends AbstractFormLoginAuthenticator implements 
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
