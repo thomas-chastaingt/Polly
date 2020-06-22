7d8886090369bccd8176e456c556a87dad9afea5
Chastaingt <thomas-chastaingt@outlook.com>
2020-06-22 16:04:15 +0200

    addMathieuPR

--- src/Security/LogginFormAuthenticator.php
+++ src/Security/LogginFormAuthenticator.php
@@ -96,7 +96,7 @@ class LogginFormAuthenticator extends AbstractFormLoginAuthenticator implements 
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('polls_new'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
