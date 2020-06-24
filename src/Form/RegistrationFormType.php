42f387177d92e07d1c02852d57173893fe0fbc22
Chastaingt <thomas-chastaingt@outlook.com>
2020-06-24 19:54:16 +0200

    add some styles in poll create and registration

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,17 +22,6 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
             ])
         ;
     }
