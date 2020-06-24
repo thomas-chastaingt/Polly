8b36755dbeb6519e5462b5df49ae45822e46e23c
Chastaingt <thomas-chastaingt@outlook.com>
2020-06-24 11:01:47 +0200

    add form new poll and PR Gabriel

--- src/Controller/TrendsController.php
+++ src/Controller/TrendsController.php
@@ -6,16 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PollsRepository;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\PollAnswersRepository;
 
 class TrendsController extends AbstractController
 {
     /**
      * @Route("/trends", name="trends")
      */
-    public function index(PollsRepository $pollsRepository):Response
+    public function index(PollsRepository $pollsRepository, PollAnswersRepository $pollAnswersRepository):Response
     {
+        $polls = $pollsRepository->findBy([],['dateCreation' => 'DESC']);
+       
+        $stats = [];
+
+        for ($i = 0; $i < count($polls); $i = $i+1) {
+            $count = count($pollAnswersRepository->findByPoll($polls[$i]));
+
+            $stats[] = [
+                'count' => $count,
+                'poll' => $polls[$i]
+            ];
+
+        }
         return $this->render('trends/index.html.twig', [
-            'polls' => $pollsRepository->findAll(),
+            'stats' => $stats
         ]);
     }
 }
