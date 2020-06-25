db15e340de06b2b3676d91eb163e8d6ddbba99b5
gaby1994 <gabriel.degez@free.fr>
2020-06-25 14:39:27 +0200

    Add files via upload

--- src/Repository/PollsRepository.php
+++ src/Repository/PollsRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Polls;
+use App\Entity\PollAnswers;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
