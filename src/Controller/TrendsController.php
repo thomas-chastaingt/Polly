21d0f7e8667cb0ae1cac2e533c432afb999bacb6
gaby1994 <gabriel.degez@free.fr>
2020-06-25 14:40:07 +0200

    Add files via upload

--- src/Controller/TrendsController.php
+++ src/Controller/TrendsController.php
@@ -5,36 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PollsRepository;
-use App\Repository\PollAnswersRepository;
-
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\PollAnswersRepository;
 
 class TrendsController extends AbstractController
 {
     /**
      * @Route("/trends", name="trends")
      */
-    public function index(PollsRepository $pollsRepository, PollAnswersRepository $pollAnswersRepository):Response
+    public function index(PollAnswersRepository $pollAnswersRepository):Response
     {
-        $polls = $pollsRepository->findBy([],['dateCreation' => 'DESC']);
-        //dd($polls);
-        $stats = [];
-        
-        for ($i = 0; $i < count($polls); $i = $i+1) {
-            $count = count($pollAnswersRepository->findByPoll($polls[$i]));
-
-            $stats[] = [
-                'count' => $count,
-                'poll' => $polls[$i]
-            ];
-                    
-        }
-
-        //arsort($stats);
+        $stats = $pollAnswersRepository->countByNumberAnswers();
         //dd($stats);
-
         return $this->render('trends/index.html.twig', [
-            'stats' => $stats      
+            'stats' => $stats
         ]);
     }
 }
